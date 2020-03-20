d85eb1be8aa6aec6b2271616c9469048f17657b1
Dylan Yung <YungDexiong@hotmail.com>
2020-03-19 22:32:29 -0700

    fixed dataset

--- Dataset/Dataset.py
+++ Dataset/Dataset.py
@@ -1,3 +1,4 @@
+import unicodedata
 from pandas import DataFrame
 from torch.utils.data import Dataset
 
@@ -5,6 +6,9 @@ from torch.utils.data import Dataset
 class Dataset(Dataset):
     def __init__(self, df: DataFrame):
         self.data_frame = df
+        self.data_frame['name'].apply(lambda val: unicodedata.normalize('NFKD', val).encode('ascii', 'ignore').decode())
+        self.data_frame['first'].apply(lambda val: unicodedata.normalize('NFKD', val).encode('ascii', 'ignore').decode())
+        self.data_frame['last'].apply(lambda val: unicodedata.normalize('NFKD', val).encode('ascii', 'ignore').decode())
 
     def __len__(self):
         return len(self.data_frame)
